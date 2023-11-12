41fee1c59f6c0887d18b325dc14ba2f8c02376e6
Nakata Nobuyuki <nobu.nakata@gmail.com>
2023-11-12 12:52:02 +0900

    パーティクル数のリセット用関数定義

--- Samples/GPUActions/Utils/GPUPropertyExt.cs
+++ Samples/GPUActions/Utils/GPUPropertyExt.cs
@@ -6,14 +6,22 @@ namespace GPUParticleSystem.Samples.GPUActions.Utils {
     public static class GPUPropertyExt {
 
         public static MaterialPropertyBlock SetParticles(this MaterialPropertyBlock matProps, GraphicsBuffer particles) {
-            matProps.SetInt(GPUActionController.P_ParticlesCount, particles.count);
-            matProps.SetBuffer(GPUActionController.P_Particles, particles);
+            if (particles == null) {
+                matProps.SetInt(GPUActionController.P_ParticlesCount, 0);
+            } else {
+                matProps.SetInt(GPUActionController.P_ParticlesCount, particles.count);
+                matProps.SetBuffer(GPUActionController.P_Particles, particles);
+            }
             return matProps;
         }
         public static GraphicsBuffer SetGlobalParticles(this GraphicsBuffer particles) {
-            Shader.SetGlobalInt(GPUActionController.P_ParticlesCount, particles.count);
+            particles.count.SetGlobalParticleCount();
             Shader.SetGlobalBuffer(GPUActionController.P_Particles, particles);
             return particles;
+        }
+        public static int SetGlobalParticleCount(this int count) {
+            Shader.SetGlobalInt(GPUActionController.P_ParticlesCount, count);
+            return count;
         }
     }
 }

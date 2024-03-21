4a107bd748d0a960c575bac5ccea274f2acc59b1
Nakata Nobuyuki <nobu.nakata@gmail.com>
2024-03-21 14:59:00 +0900

    重複ファイル削除

--- Runtime/Constants/ShaderIDs.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace GPUParticleSystem.Constants {
-
-    public static class ShaderIDs {
-        public static readonly int P_ParticlesCount = Shader.PropertyToID("_ParticlesCount");
-        public static readonly int P_Particles = Shader.PropertyToID("_Particles");
-    }
-}
